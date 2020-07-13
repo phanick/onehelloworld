bdce8f0eece90d45d349ce4e14a12f8dbb4578e9
phanick <39258517+phanick@users.noreply.github.com>
2020-07-13 16:46:36 +0530

    Create firstapptest.java

---
+++ myoneapp/src/test/java/com/myonecompany/app/firstapptest.java
@@ -0,0 +1,38 @@
+package com.myonecompany.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class firstapptest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public firstapptest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
